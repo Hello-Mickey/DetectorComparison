8bb2e1d8a3021673ac95e3d14415856a202522a9
MikeMishchanin <mishchanin.mihael@gmail.com>
2019-05-19 14:29:48 +0300

    check stability

--- Comparing.cpp
+++ Comparing.cpp
@@ -6,16 +6,21 @@
 #include <iomanip>
 #include <string>
 
+
+typedef cv::Vec<uchar, 3> Vec3b;
+
+
 const int THRESHOLD_FAST = 50;
 const int THRESHOLD_HARRIS = 175;
-const int PIXEL_EPS = 3;
+const int PIXEL_EPS = 2;
 const int eps = PIXEL_EPS * PIXEL_EPS;
+std::string result_path = "./results/";
 
-void DrawPoints(cv::Mat& img, std::vector<cv::KeyPoint>& kpts) {
+void DrawPoints(cv::Mat& img, std::vector<cv::KeyPoint>& kpts, cv::Scalar color) {
 	for (int i = 0; i < kpts.size(); ++i) {
 		cv::Point point = kpts[i].pt;
 		//std::cout << "X: " << point.x << "    Y: " << point.y << '\n';
-		cv::circle(img, point, 2, cv::Scalar(0, 0, 255), -1, 8, 0);
+		cv::circle(img, point, 2, color, -1, 8, 0);
 	}
 }
 
@@ -33,8 +38,10 @@ void CornerDetector(std::string type, cv::Mat& img, cv::Mat& img_out, std::vecto
 		cv::Ptr<cv::BRISK> brisk = cv::BRISK::create();
 		brisk->detectAndCompute(img, cv::Mat(), kpts, desc);
 	}
-	DrawPoints(img_out, kpts);
+	DrawPoints(img_out, kpts, cv::Scalar(0, 0, 255));
 	std::cout << type << '\n' << kpts.size() << '\n';
+	std::string path = "./results/" + type + ".jpg";
+	cv::imwrite(path, img_out);
 }
 
 
@@ -50,7 +57,7 @@ bool len2(cv::Point& point1, cv::Point& point2) {
 }
 
 void merge(std::vector<cv::KeyPoint>& kpts1, std::vector<cv::KeyPoint>& kpts2, std::vector<cv::KeyPoint>& kpts_merge) {
-	//std::cout << "keypoints count 1: " << kpts1.size() << '\n' << "keypoints count 2: " << kpts2.size() << '\n';
+	std::cout << "keypoints count 1: " << kpts1.size() << '\n' << "keypoints count 2: " << kpts2.size() << '\n';
 
 
 	for (int i = 0; i < kpts1.size(); ++i) {
@@ -64,58 +71,101 @@ void merge(std::vector<cv::KeyPoint>& kpts1, std::vector<cv::KeyPoint>& kpts2, s
 			}
 		}
 	}
-	std::cout << kpts_merge.size();
+	std::cout << kpts_merge.size() << '\n';
 }
 
 
-int main() {
-	cv::Mat img, gray, thresh, img_fast, img_orb, desc_orb, img_merge, img_brisk, desc_brisk;
-	std::vector<cv::KeyPoint> kpts_fast, kpts_orb, kpts_merge, kpts_brisk;
 
-	img = cv::imread("./imgs/Number.jpg");
+
+
+void init(cv::Mat& img, cv::Mat& gray, cv::Mat& img_fast, cv::Mat& img_orb, cv::Mat& img_merge, cv::Mat& img_brisk) {
 	img.copyTo(img_fast);
 	img.copyTo(img_orb);
 	img.copyTo(img_merge);
 	img.copyTo(img_brisk);
-
 	cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
-	cv::threshold(gray, thresh, THRESHOLD_HARRIS, 255, cv::THRESH_BINARY);
+}
+//example_16 - 02.cpp
+typedef cv::Vec<uchar, 2> Vec2b;
+void printMat(cv::Mat& rot) {
+	std::cout << rot.rows << " " << rot.cols << '\n';
+	for (int i = 0; i < rot.rows; ++i) {
+		for (int j = 0; j < rot.cols; ++j) {
+			std::cout << rot.at<double>(i, j) << ' ';
+
+		}
+		std::cout << '\n';
+	}
+}
+
+std::vector<cv::KeyPoint> CoordTransform(std::vector<cv::KeyPoint>& k,  cv::Mat& tr) {
+	std::vector<cv::KeyPoint> kr = k;
+	for (int i = 0; i < k.size(); ++i) {
+		kr[i].pt.x = tr.at<double>(0, 0) * k[i].pt.x + tr.at<double>(0, 1) * k[i].pt.y + tr.at<double>(0, 2);
+		kr[i].pt.y = tr.at<double>(1, 0) * k[i].pt.x + tr.at<double>(1, 1) * k[i].pt.y + tr.at<double>(1, 2);
+		
+		//std::cout << kr[i].pt.x << " " << kr[i].pt.y<<'\n';
+	}
+	return kr;
+}
+
+
+
+
+
+
+int main() {
+	cv::Mat img, gray, thresh, img_fast, img_orb, desc_orb, img_merge, img_brisk, desc_brisk;
+	std::vector<cv::KeyPoint> kpts_fast, kpts_orb, kpts_merge, kpts_brisk, kpts_merge_brisk_orb;
+
+	cv::Mat img2, gray2, thresh2, img_fast2, img_orb2, desc_orb2, img_merge2, img_brisk2, desc_brisk2, dst;
+	std::vector<cv::KeyPoint> kpts_fast2, kpts_orb2, kpts_merge2, kpts_brisk2, kpts_merge_brisk_orb2;
+
+	cv::Mat img_transform, img_transform_merge;
+	std::vector<cv::KeyPoint> kpts_orb_transform, kpts_orb_transform_merge;
+	double angle = 180;
+
+	img = cv::imread("./imgs/Number.jpg");
+	img2 = cv::imread("./imgs/Number180.jpg");
+
+	img2.copyTo(img_transform);
+	img2.copyTo(img_transform_merge);
+
+	init(img, gray, img_fast, img_orb, img_merge, img_brisk);
+	init(img2, gray2, img_fast2, img_orb2, img_merge2, img_brisk2);
+	cv::Mat M = cv::getRotationMatrix2D(cv::Point(img.cols / 2.0, img.rows / 2.0), 180, 1);
+	printMat(M);
 
 	CornerDetector("orb", gray, img_orb, kpts_orb, desc_orb);
-	CornerDetector("fast", gray, img_fast, kpts_fast, desc_orb);
-	CornerDetector("brisk", gray, img_brisk, kpts_brisk, desc_brisk);
+	CornerDetector("orb", gray2, img_orb2, kpts_orb2, desc_orb2);
 
-	cv::imshow("ORB",img_orb);
-	cv::imshow("FAST", img_fast);
-	cv::imshow("BRISK", img_brisk);
-
-	cv::imwrite("./results/ORB.jpg", img);
-
-	merge(kpts_brisk, kpts_orb, kpts_merge);
+	DrawPoints(img_orb, kpts_orb, cv::Scalar(0, 0, 255));//source1
+	DrawPoints(img_orb2, kpts_orb2, cv::Scalar(0, 0, 255));//source2
+	cv::imshow("ORB_1", img_orb);
+	cv::imshow("ORB_2", img_orb2);
+	kpts_orb_transform = CoordTransform(kpts_orb, M);
+	DrawPoints(img_transform, kpts_orb_transform, cv::Scalar(0, 255, 0));//transform copy source2
 	
-	DrawPoints(img_merge, kpts_merge);
-	cv::imshow("MERGE", img_merge);
+	merge(kpts_orb_transform, kpts_orb2, kpts_orb_transform_merge);
+	DrawPoints(img_orb2, kpts_orb_transform_merge, cv::Scalar(0, 255, 0));//transform source2
+
+	DrawPoints(img_transform_merge, kpts_orb_transform_merge, cv::Scalar(0, 255, 0));//transform source2
+
 	
+	cv::imshow("ORB_2_transform", img_transform);
+//	cv::imshow("ORB_transform_merge1", img_transform_merge);
+	cv::imshow("ORB_transform_merge2", img_orb2);
+
+//	std::string path = result_path + "transformMerge.jpg";
+//	cv::imwrite(path, img_orb2);
 	cv::waitKey(0);
+
+
+
+
 
 }
 
-//cv::Mat harris(cv::Mat img) {
-//	cv::Mat dst;
-////	cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
-//	cv::cornerHarris(img, dst,2,3,0.04);
-//	cv::normalize(dst, dst, 0, 255, cv::NORM_MINMAX, CV_32FC1);
-//	return dst;
-//}
 
-//	dst = harris(gray);
-//	for (int j = 0; j < dst.rows; j++) {
-//		for (int i = 0; i < dst.cols; i++) {
-//			if ((int)dst.at<float>(j, i) > THRESHOLD_HARRIS) {
-//
-//				cv::circle(img, cv::Point(i, j), 2, cv::Scalar(0, 0, 255), -1, 8, 0);
-//			}
-//		}
-//	}
-//	cv::imshow("harris", img);
-//	cv::waitKey(0);
+
+
